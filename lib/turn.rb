989baca8ca2ba42e87778786ae6868fa2c18df46
Isaiah Magett <imagett10@gmail.com>
2019-01-20 17:29:22 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,7 +15,7 @@ def valid_move?(board,index)
 end
 
 def position_taken?(board,index) 
-  board[index] == "X" board[index] == "O"
+  board[index] == "X" || board[index] == "O"
 def move()
 end
 def turn()
