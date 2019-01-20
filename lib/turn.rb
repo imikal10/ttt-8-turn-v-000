bd4f996f4f388a8b88f64f1764094b2c99719bb6
Isaiah Magett <imagett10@gmail.com>
2019-01-20 17:36:17 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,7 +18,7 @@ def position_taken?(board,index)
   board[index] == "X" || board[index] == "O"
 end
 
-def move(board,index,token("X"))
+def move(board,index,token = "X")
   board[index] = token 
 end
 
